4997a0facb5abbc0faf964f595b36844b6922c61
Sarah Santos <sarahsayonara.santos@gmail.com>
2025-06-20 18:08:29 +0000

    Respostas adicionadas

---
+++ pratica01/multiplicacao_com_expo.py
@@ -0,0 +1,12 @@
+# 3- Calculadora de Volume
+# Crie um programa que calcula o volume de uma caixa retangular. Use as seguintes dimensões:
+
+comprimento = 12
+largura = 14
+altura = 20
+
+# Calcula o volume da caixa
+volume = comprimento * largura * altura
+
+# Exibe o resultado
+print(f"O volume da caixa é {volume} cm³")
